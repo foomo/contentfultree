a105ee9d31e172ed0a8dd6a4ec763097ad06d27e
Nicola Turcato <nicola.turcato@bestbytes.com>
2025-08-29 08:55:33 +0200

    feat: update deps

--- dist/ContentTree.styled.js
+++ dist/ContentTree.styled.js
@@ -1,139 +1,37 @@
-'use strict'
-var __makeTemplateObject =
-	(this && this.__makeTemplateObject) ||
-	function (cooked, raw) {
-		if (Object.defineProperty) {
-			Object.defineProperty(cooked, 'raw', { value: raw })
-		} else {
-			cooked.raw = raw
-		}
-		return cooked
-	}
-var __importDefault =
-	(this && this.__importDefault) ||
-	function (mod) {
-		return mod && mod.__esModule ? mod : { default: mod }
-	}
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.StyledSpinner =
-	exports.StyledContentTreeNodePublishingStatus =
-	exports.StyledContentTreeNodeName =
-	exports.StyledContentTreeNodeWedge =
-	exports.StyledContentTreeTableNodeCell =
-	exports.StyledContentTreeTable =
-		void 0
-var styled_components_1 = __importDefault(require('styled-components'))
-exports.StyledContentTreeTable = styled_components_1.default.table(
-	templateObject_1 ||
-		(templateObject_1 = __makeTemplateObject(
-			[
-				'\n\twidth: 100%;\n\tcolor: black;\n\tborder: 0;\n\tpadding: 0 60px;\n\tmargin: 0 auto;\n\ttd {\n\t\tpadding: 0.2em 1em 0.2em 0.2em;\n\t\tcolor: #999999;\n\t\tborder-bottom: 1px solid #efefef;\n\t}\n\tth {\n\t\ttext-align: left;\n\t\tfont-weight: normal;\n\t\tfont-size: 85%;\n\t\tcolor: #666666;\n\t\tbackground: #efefef;\n\t\tpadding: 0.2em;\n\t}\n\tth.first {\n\t\tfont-weight: bold !important;\n\t\tcolor: black !important;\n\t}\n',
-			],
-			[
-				'\n\twidth: 100%;\n\tcolor: black;\n\tborder: 0;\n\tpadding: 0 60px;\n\tmargin: 0 auto;\n\ttd {\n\t\tpadding: 0.2em 1em 0.2em 0.2em;\n\t\tcolor: #999999;\n\t\tborder-bottom: 1px solid #efefef;\n\t}\n\tth {\n\t\ttext-align: left;\n\t\tfont-weight: normal;\n\t\tfont-size: 85%;\n\t\tcolor: #666666;\n\t\tbackground: #efefef;\n\t\tpadding: 0.2em;\n\t}\n\tth.first {\n\t\tfont-weight: bold !important;\n\t\tcolor: black !important;\n\t}\n',
-			],
-		)),
-)
-exports.StyledContentTreeTableNodeCell = styled_components_1.default.td(
-	templateObject_2 ||
-		(templateObject_2 = __makeTemplateObject(
-			[
-				'\n\tpadding-left: ',
-				'em !important;\n\tpadding-right: 2em !important;\n\tcolor: black !important;\n\tmin-width: 450px !important;\n',
-			],
-			[
-				'\n\tpadding-left: ',
-				'em !important;\n\tpadding-right: 2em !important;\n\tcolor: black !important;\n\tmin-width: 450px !important;\n',
-			],
-		)),
-	function (props) {
-		return props.$depth
-	},
-)
-exports.StyledContentTreeNodeWedge = styled_components_1.default.div(
-	templateObject_3 ||
-		(templateObject_3 = __makeTemplateObject(
-			[
-				'\n\tdisplay: inline-block;\n\twidth: 1em;\n\ttext-align: left;\n\ta {\n\t\tcursor: pointer;\n\t\tfont-size: 130%;\n\t\tline-height: 100%;\n\t}\n',
-			],
-			[
-				'\n\tdisplay: inline-block;\n\twidth: 1em;\n\ttext-align: left;\n\ta {\n\t\tcursor: pointer;\n\t\tfont-size: 130%;\n\t\tline-height: 100%;\n\t}\n',
-			],
-		)),
-)
-exports.StyledContentTreeNodeName = styled_components_1.default.div(
-	templateObject_4 ||
-		(templateObject_4 = __makeTemplateObject(
-			[
-				'\n\tdisplay: inline-block;\n\ta {\n\t\tcursor: pointer;\n\t}\n\ta:hover {\n\t\ttext-decoration: underline;\n\t}\n',
-			],
-			[
-				'\n\tdisplay: inline-block;\n\ta {\n\t\tcursor: pointer;\n\t}\n\ta:hover {\n\t\ttext-decoration: underline;\n\t}\n',
-			],
-		)),
-)
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StyledSpinner = exports.StyledContentTreeNodePublishingStatus = exports.StyledContentTreeNodeName = exports.StyledContentTreeNodeWedge = exports.StyledContentTreeTableNodeCell = exports.StyledContentTreeTable = void 0;
+var styled_components_1 = __importDefault(require("styled-components"));
+exports.StyledContentTreeTable = styled_components_1.default.table(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n\twidth: 100%;\n\tcolor: black;\n\tborder: 0;\n\tpadding: 0 60px;\n\tmargin: 0 auto;\n\ttd {\n\t\tpadding: 0.2em 1em 0.2em 0.2em;\n\t\tcolor: #999999;\n\t\tborder-bottom: 1px solid #efefef;\n\t}\n\tth {\n\t\ttext-align: left;\n\t\tfont-weight: normal;\n\t\tfont-size: 85%;\n\t\tcolor: #666666;\n\t\tbackground: #efefef;\n\t\tpadding: 0.2em;\n\t}\n\tth.first {\n\t\tfont-weight: bold !important;\n\t\tcolor: black !important;\n\t}\n"], ["\n\twidth: 100%;\n\tcolor: black;\n\tborder: 0;\n\tpadding: 0 60px;\n\tmargin: 0 auto;\n\ttd {\n\t\tpadding: 0.2em 1em 0.2em 0.2em;\n\t\tcolor: #999999;\n\t\tborder-bottom: 1px solid #efefef;\n\t}\n\tth {\n\t\ttext-align: left;\n\t\tfont-weight: normal;\n\t\tfont-size: 85%;\n\t\tcolor: #666666;\n\t\tbackground: #efefef;\n\t\tpadding: 0.2em;\n\t}\n\tth.first {\n\t\tfont-weight: bold !important;\n\t\tcolor: black !important;\n\t}\n"])));
+exports.StyledContentTreeTableNodeCell = styled_components_1.default.td(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n\tpadding-left: ", "em !important;\n\tpadding-right: 2em !important;\n\tcolor: black !important;\n\tmin-width: 450px !important;\n"], ["\n\tpadding-left: ", "em !important;\n\tpadding-right: 2em !important;\n\tcolor: black !important;\n\tmin-width: 450px !important;\n"])), function (props) { return props.$depth; });
+exports.StyledContentTreeNodeWedge = styled_components_1.default.div(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n\tdisplay: inline-block;\n\twidth: 1em;\n\ttext-align: left;\n\ta {\n\t\tcursor: pointer;\n\t\tfont-size: 130%;\n\t\tline-height: 100%;\n\t}\n"], ["\n\tdisplay: inline-block;\n\twidth: 1em;\n\ttext-align: left;\n\ta {\n\t\tcursor: pointer;\n\t\tfont-size: 130%;\n\t\tline-height: 100%;\n\t}\n"])));
+exports.StyledContentTreeNodeName = styled_components_1.default.div(templateObject_4 || (templateObject_4 = __makeTemplateObject(["\n\tdisplay: inline-block;\n\ta {\n\t\tcursor: pointer;\n\t}\n\ta:hover {\n\t\ttext-decoration: underline;\n\t}\n"], ["\n\tdisplay: inline-block;\n\ta {\n\t\tcursor: pointer;\n\t}\n\ta:hover {\n\t\ttext-decoration: underline;\n\t}\n"])));
 var getPublishingStatusColors = function (status) {
-	switch (status) {
-		case 'draft':
-			return {
-				publishingStatusBg: 'rgb(253, 229, 192)',
-				publishingStatusFg: 'rgb(177, 45, 0)',
-			}
-		case 'changed':
-			return {
-				publishingStatusBg: 'rgb(206, 236, 255)',
-				publishingStatusFg: 'rgb(0, 89, 200)',
-			}
-		case 'published':
-			return {
-				publishingStatusBg: 'rgb(205, 243, 198)',
-				publishingStatusFg: 'rgb(0, 109, 35)',
-			}
-	}
-}
-exports.StyledContentTreeNodePublishingStatus = styled_components_1.default.div(
-	templateObject_5 ||
-		(templateObject_5 = __makeTemplateObject(
-			[
-				'\n\tdisplay: inline-block;\n\tfont-family:\n\t\t-apple-system,\n\t\tBlinkMacSystemFont,\n\t\tSegoe UI,\n\t\tHelvetica,\n\t\tArial,\n\t\tsans-serif,\n\t\tApple Color Emoji,\n\t\tSegoe UI Emoji,\n\t\tSegoe UI Symbol;\n\tfont-weight: 600;\n\tfont-size: 80%;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.06rem;\n\tborder-radius: 4px;\n\tpadding: 0 0.2rem;\n\tcolor: ',
-				' !important;\n\tbackground-color: ',
-				' !important;\n',
-			],
-			[
-				'\n\tdisplay: inline-block;\n\tfont-family:\n\t\t-apple-system,\n\t\tBlinkMacSystemFont,\n\t\tSegoe UI,\n\t\tHelvetica,\n\t\tArial,\n\t\tsans-serif,\n\t\tApple Color Emoji,\n\t\tSegoe UI Emoji,\n\t\tSegoe UI Symbol;\n\tfont-weight: 600;\n\tfont-size: 80%;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.06rem;\n\tborder-radius: 4px;\n\tpadding: 0 0.2rem;\n\tcolor: ',
-				' !important;\n\tbackground-color: ',
-				' !important;\n',
-			],
-		)),
-	function (props) {
-		var _a
-		return (_a = getPublishingStatusColors(props.$status)) === null ||
-			_a === void 0
-			? void 0
-			: _a.publishingStatusFg
-	},
-	function (props) {
-		var _a
-		return (_a = getPublishingStatusColors(props.$status)) === null ||
-			_a === void 0
-			? void 0
-			: _a.publishingStatusBg
-	},
-)
-exports.StyledSpinner = styled_components_1.default.div(
-	templateObject_6 ||
-		(templateObject_6 = __makeTemplateObject(
-			[
-				'\n\tdisplay: inline-block;\n\twidth: 1em;\n\tanimation: rotate 1s infinite;\n\t@keyframes rotate {\n\t\tfrom {\n\t\t\ttransform: rotate(0deg);\n\t\t}\n\t\tto {\n\t\t\ttransform: rotate(360deg);\n\t\t}\n\t}\n',
-			],
-			[
-				'\n\tdisplay: inline-block;\n\twidth: 1em;\n\tanimation: rotate 1s infinite;\n\t@keyframes rotate {\n\t\tfrom {\n\t\t\ttransform: rotate(0deg);\n\t\t}\n\t\tto {\n\t\t\ttransform: rotate(360deg);\n\t\t}\n\t}\n',
-			],
-		)),
-)
-var templateObject_1,
-	templateObject_2,
-	templateObject_3,
-	templateObject_4,
-	templateObject_5,
-	templateObject_6
+    switch (status) {
+        case 'draft':
+            return {
+                publishingStatusBg: 'rgb(253, 229, 192)',
+                publishingStatusFg: 'rgb(177, 45, 0)',
+            };
+        case 'changed':
+            return {
+                publishingStatusBg: 'rgb(206, 236, 255)',
+                publishingStatusFg: 'rgb(0, 89, 200)',
+            };
+        case 'published':
+            return {
+                publishingStatusBg: 'rgb(205, 243, 198)',
+                publishingStatusFg: 'rgb(0, 109, 35)',
+            };
+    }
+};
+exports.StyledContentTreeNodePublishingStatus = styled_components_1.default.div(templateObject_5 || (templateObject_5 = __makeTemplateObject(["\n\tdisplay: inline-block;\n\tfont-family:\n\t\t-apple-system,\n\t\tBlinkMacSystemFont,\n\t\tSegoe UI,\n\t\tHelvetica,\n\t\tArial,\n\t\tsans-serif,\n\t\tApple Color Emoji,\n\t\tSegoe UI Emoji,\n\t\tSegoe UI Symbol;\n\tfont-weight: 600;\n\tfont-size: 80%;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.06rem;\n\tborder-radius: 4px;\n\tpadding: 0 0.2rem;\n\tcolor: ", " !important;\n\tbackground-color: ", " !important;\n"], ["\n\tdisplay: inline-block;\n\tfont-family:\n\t\t-apple-system,\n\t\tBlinkMacSystemFont,\n\t\tSegoe UI,\n\t\tHelvetica,\n\t\tArial,\n\t\tsans-serif,\n\t\tApple Color Emoji,\n\t\tSegoe UI Emoji,\n\t\tSegoe UI Symbol;\n\tfont-weight: 600;\n\tfont-size: 80%;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.06rem;\n\tborder-radius: 4px;\n\tpadding: 0 0.2rem;\n\tcolor: ", " !important;\n\tbackground-color: ", " !important;\n"])), function (props) { var _a; return (_a = getPublishingStatusColors(props.$status)) === null || _a === void 0 ? void 0 : _a.publishingStatusFg; }, function (props) { var _a; return (_a = getPublishingStatusColors(props.$status)) === null || _a === void 0 ? void 0 : _a.publishingStatusBg; });
+exports.StyledSpinner = styled_components_1.default.div(templateObject_6 || (templateObject_6 = __makeTemplateObject(["\n\tdisplay: inline-block;\n\twidth: 1em;\n\tanimation: rotate 1s infinite;\n\t@keyframes rotate {\n\t\tfrom {\n\t\t\ttransform: rotate(0deg);\n\t\t}\n\t\tto {\n\t\t\ttransform: rotate(360deg);\n\t\t}\n\t}\n"], ["\n\tdisplay: inline-block;\n\twidth: 1em;\n\tanimation: rotate 1s infinite;\n\t@keyframes rotate {\n\t\tfrom {\n\t\t\ttransform: rotate(0deg);\n\t\t}\n\t\tto {\n\t\t\ttransform: rotate(360deg);\n\t\t}\n\t}\n"])));
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6;
--- dist/ContentTreeUtils.js
+++ dist/ContentTreeUtils.js
@@ -1,92 +1,67 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.cfEntriesToNodes = exports.emptyNodeProps = void 0
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cfEntriesToNodes = exports.emptyNodeProps = void 0;
 var emptyNodeProps = function () {
-	return { id: '', name: '', expand: false, parentId: '' }
-}
-exports.emptyNodeProps = emptyNodeProps
+    return { id: '', name: '', expand: false, parentId: '' };
+};
+exports.emptyNodeProps = emptyNodeProps;
 var cfEntryHasChildren = function (entry, nodeContentTypes, locales) {
-	var _a
-	for (
-		var _i = 0, nodeContentTypes_1 = nodeContentTypes;
-		_i < nodeContentTypes_1.length;
-		_i++
-	) {
-		var nodeContentType = nodeContentTypes_1[_i]
-		for (var _b = 0, locales_1 = locales; _b < locales_1.length; _b++) {
-			var locale = locales_1[_b]
-			if (
-				(_a = entry.fields[nodeContentType]) === null || _a === void 0
-					? void 0
-					: _a[locale]
-			) {
-				return true
-			}
-		}
-	}
-	return false
-}
+    var _a;
+    for (var _i = 0, nodeContentTypes_1 = nodeContentTypes; _i < nodeContentTypes_1.length; _i++) {
+        var nodeContentType = nodeContentTypes_1[_i];
+        for (var _b = 0, locales_1 = locales; _b < locales_1.length; _b++) {
+            var locale = locales_1[_b];
+            if ((_a = entry.fields[nodeContentType]) === null || _a === void 0 ? void 0 : _a[locale]) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
 var cfEntryPublishingStatus = function (entry) {
-	if (!entry.sys.publishedVersion) {
-		return 'draft'
-	}
-	if (entry.sys.version - entry.sys.publishedVersion === 1) {
-		return 'published'
-	}
-	return 'changed'
-}
-var cfEntriesToNodes = function (
-	entries,
-	titleFields,
-	stLocale,
-	locales,
-	nodeContentTypes,
-	iconRegistry,
-	parentId,
-) {
-	if (entries.length === 0) {
-		return []
-	}
-	var nodeArray = []
-	entries.forEach(function (entry) {
-		var _a
-		if (!entry) {
-			return
-		}
-		var name = ''
-		for (
-			var _i = 0, titleFields_1 = titleFields;
-			_i < titleFields_1.length;
-			_i++
-		) {
-			var titleField = titleFields_1[_i]
-			if (
-				(_a = entry.fields[titleField]) === null || _a === void 0
-					? void 0
-					: _a[stLocale]
-			) {
-				name = entry.fields[titleField][stLocale]
-				break
-			}
-		}
-		if (name === '') {
-			name = entry.sys.id
-		}
-		var node = {
-			id: entry.sys.id,
-			name: name,
-			contentType: entry.sys.contentType.sys.id,
-			icon:
-				iconRegistry != null ? iconRegistry[entry.sys.contentType.sys.id] : '',
-			expand: !!parentId,
-			parentId: parentId,
-			hasChildNodes: cfEntryHasChildren(entry, nodeContentTypes, locales),
-			publishingStatus: cfEntryPublishingStatus(entry),
-			updatedAt: entry.sys.updatedAt,
-			publishedAt: entry.sys.publishedAt,
-		}
-		nodeArray.push(node)
-	})
-	return nodeArray
-}
-exports.cfEntriesToNodes = cfEntriesToNodes
+    if (!entry.sys.publishedVersion) {
+        return 'draft';
+    }
+    if (entry.sys.version - entry.sys.publishedVersion === 1) {
+        return 'published';
+    }
+    return 'changed';
+};
+var cfEntriesToNodes = function (entries, titleFields, stLocale, locales, nodeContentTypes, iconRegistry, parentId) {
+    if (entries.length === 0) {
+        return [];
+    }
+    var nodeArray = [];
+    entries.forEach(function (entry) {
+        var _a;
+        if (!entry) {
+            return;
+        }
+        var name = '';
+        for (var _i = 0, titleFields_1 = titleFields; _i < titleFields_1.length; _i++) {
+            var titleField = titleFields_1[_i];
+            if ((_a = entry.fields[titleField]) === null || _a === void 0 ? void 0 : _a[stLocale]) {
+                name = entry.fields[titleField][stLocale];
+                break;
+            }
+        }
+        if (name === '') {
+            name = entry.sys.id;
+        }
+        var node = {
+            id: entry.sys.id,
+            name: name,
+            contentType: entry.sys.contentType.sys.id,
+            icon: iconRegistry != null ? iconRegistry[entry.sys.contentType.sys.id] : '',
+            expand: !!parentId,
+            parentId: parentId,
+            hasChildNodes: cfEntryHasChildren(entry, nodeContentTypes, locales),
+            publishingStatus: cfEntryPublishingStatus(entry),
+            updatedAt: entry.sys.updatedAt,
+            publishedAt: entry.sys.publishedAt,
+        };
+        nodeArray.push(node);
+    });
+    return nodeArray;
+};
+exports.cfEntriesToNodes = cfEntriesToNodes;
